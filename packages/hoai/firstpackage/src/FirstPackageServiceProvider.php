cd6069fd4a93f50a2c5333f16140e722bcb925ca
Hoai <lamthanhhoai1990@gmail.com>
2019-12-14 22:33:21 +0700

    setting package

---
+++ packages/hoai/firstpackage/src/FirstPackageServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+namespace hoai\firstpackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class FirstPackageServiceProvider extends ServiceProvider {
+	public function boot()
+	{
+
+	}
+	public function register()
+	{
+
+	}
+}
